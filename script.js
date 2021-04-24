f08613fd1c154a18986a5b310652b64d109b30bd
GERMAN MARTINEZ <germanmartinezgross@outlook.com>
2021-04-24 14:10:44 -0300

    test

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ const classNames = {
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
   function addTodo() {
-    alert('Boton Add TODO clickeado!')
+    //alert('Boton Add TODO clickeado!')
+    //itemCountSpan = 1;
+    let texto = prompt('Escriba el nombre del to-do')
+    if(texto != null){
+      if(texto != ''){
+        agregarLinea(texto)
+      }
+    }
+  }
+
+  function agregarLinea(texto){
+    let linea = document.createElement('li')
+    let texto = document.createTextNode(texto)
+    texto.nodeType = "checkbox"
+    linea.nodeType = "checkbox"
+    linea.appendChild(texto)
+    list.appendChild(linea)
   }
